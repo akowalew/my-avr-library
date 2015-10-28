8fe4905cb34c37e288a162625a2e8961c03fd5e9
akowalew <ram.techen@gmail.com>
2015-10-28 21:01:31 +0100

    Wprowadzenie przestrzenii nazw. Dodanie m328p

--- ADC.h
+++ ADC.h
@@ -8,57 +8,65 @@
 #ifndef ADC_H_
 #define ADC_H_
 
-enum ADC_V_REF
+namespace Adc
 {
-	ADC_V_REF_AREF = 0x00,		// AREF, Internal Vref turned off
-	ADC_V_REF_AVCC = 0x01,  	// AVCC with external capacitor at AREF pin
-	ADC_V_REF_INTERNAL= 0x03	// Internal 2,56V Voltage reference with external capacitor at AREF pin
-};
+
+	enum V_REF
+	{
+		VREF_AREF = 0x00,	// AREF, Internal Vref turned off
+		VREF_AVCC = 0x01,  	// AVCC with external capacitor at AREF pin
+		VREF_INTERNAL= 0x03	// Internal 1.1V Voltage reference with external capacitor at AREF pin
+	};
+
+	enum PRESCALER
+	{
+		PS_2 = 0x01,
+		PS_4 = 0x02,
+		PS_8 = 0x03,
+		PS_16 = 0x04,
+		PS_32 = 0x05,
+		PS_64 = 0x06,
+		PS_128 = 0x07
+	};
+
+	/*
+	 * By default, the successive approximation circuitry requires an input clock frequency
+	 * between 50kHz and 200kHz to get maximum resolution. If a lower resolution than
+	 * 10 bits is needed, the input clock frequency to the ADC can be higher than 200kHz
+	 * to get a higher sample rate.
+	 */
+
+	enum ADC_AUTO_TRIGGER_SRC
+	{
+		TRG_FREE_RUN = 0x00,		// Free Running mode
+		TRG_ANA_COMP = 0x01,		// Analog Comparator
+		TRG_EXT_INT0 = 0x02,		// External interrupt Request 0
+		TRG_TC0_COMP_MATCH_A = 0x03,	// Timer/Counter0 Compare Match
+		TRG_TC0_OVF = 0x04,			// Timer/Counter0 Overflow
+		TRG_TC1_COMP_MATCH_B = 0x05,// Timer/Counter1 Compare Match B
+		TRG_TC1_OVF = 0x06,			// Timer/Counter1 Overflow
+		TRG_TC1_CPT_EVENT = 0x07 	// Timer/Counter1 Capture event
+	};
+
+	void adcInit(	V_REF v_ref = VREF_AVCC,
+					PRESCALER ps = PS_2,
+					bool toLeft = false) ;
+
+	void setAutoTrigger(bool isSet, ADC_AUTO_TRIGGER_SRC src) ;
+	void selectChannel(uint8_t channel) ;
+	void waitForEnd() ;
+	uint16_t getResult() ;
 
 
-enum ADC_PRESCALER
-{
-	ADC_PS_2 = 0x01,
-	ADC_PS_4 = 0x02,
-	ADC_PS_8 = 0x03,
-	ADC_PS_16 = 0x04,
-	ADC_PS_32 = 0x05,
-	ADC_PS_64 = 0x06,
-	ADC_PS_128 = 0x07
-};
 
-/*
- * By default, the successive approximation circuitry requires an input clock frequency
- * between 50kHz and 200kHz to get maximum resolution. If a lower resolution than
- * 10 bits is needed, the input clock frequency to the ADC can be higher than 200kHz
- * to get a higher sample rate.
- */
+	inline void intEnable() 		{ ADCSRA |= (1 << ADIE) ; }
+	inline void intDisable() 		{ ADCSRA &= ~(1 << ADIE) ; }
 
-enum ADC_AUTO_TRIGGER_SRC
-{
-	ADC_TRG_FREE_RUN = 0x00,		// Free Running mode
-	ADC_TRG_ANA_COMP = 0x01,		// Analog Comparator
-	ADC_TRG_EXT_INT0 = 0x02,		// External interrupt Request 0
-	ADC_TRG_TC0_COMP_MATCH = 0x03,	// Timer/Counter0 Compare Match
-	ADC_TRG_TC0_OVF = 0x04,			// Timer/Counter0 Overflow
-	ADC_TRG_TC1_COMP_MATCH_B = 0x05,// Timer/Counter1 Compare Match B
-	ADC_TRG_TC1_OVF = 0x06,			// Timer/Counter1 Overflow
-	ADC_TRG_TC1_CPT_EVENT = 0x07 	// Timer/Counter1 Capture event
-};
+	inline void adcEnable() 		{ ADCSRA |= (1 << ADEN) ; }
+	inline void adcDisable() 		{ ADCSRA &= ~(1 << ADEN) ; }
+	inline void startConversion() 	{ ADCSRA |= (1 << ADSC) ; }
 
-void ADC_init(	ADC_V_REF v_ref = ADC_V_REF_AVCC,
-				ADC_PRESCALER ps  = ADC_PS_2,
-				uint8_t to_left = false) ;
+	uint16_t singleRead();
+}
 
-void ADC_enable_auto_trigger(ADC_AUTO_TRIGGER_SRC src) ;
-void ADC_select_channel(uint8_t channel) ;
-void ADC_wait_for_end() ;
-uint16_t ADC_get_result() ;
-void ADC_enable_interrupt(void (*fun_handle)()) ;
-
-#define ADC_enable()				ADCSRA |= (1 << ADEN)
-#define ADC_disable() 				ADCSRA &= ~(1 << ADEN)
-#define ADC_start_conversion() 		ADCSRA |= (1 << ADSC)
-#define ADC_disable_interrupt() 	ADCSRA &= ~(1 << ADIE)
-#define ADC_disable_auto_trigger	ADCSRA &= ~(1 << ADATE)
 #endif /* ADC_H_ */
